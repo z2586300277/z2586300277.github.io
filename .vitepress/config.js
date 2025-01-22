d2474d3d4e2145980f846bf4d71ab5c6f173f86c
zhangjingguang <2586300277@qq.com>
2025-01-22 17:24:08 +0800

    js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,6 +4,21 @@ export default defineConfig({
   title: "优雅永不过时",
   base: './',
   description: "优雅永不过时的个人主页",
+  head: [
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-LKJQBJNGVF' }],
+    ['script', { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8697430839896878', crossorigin: 'anonymous' }],
+    ['script', {}, `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-LKJQBJNGVF');`],
+    ['script', {}, `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?85aef82369b0fe002f0e62a775344e89";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();`]
+  ],
   themeConfig: {
     logo: 'https://z2586300277.github.io/three-editor/dist/site.png',
     nav: [
