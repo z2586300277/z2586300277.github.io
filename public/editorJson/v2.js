6b53cabe34800bdd99b00ace85b44f88b6391ad8
zhangjingguang <2586300277@qq.com>
2025-09-24 14:04:23 +0800

    无人机定位动画

--- public/editorJson/v2.js
+++ public/editorJson/v2.js
@@ -1,5 +1,6 @@
 window.addon_editor_json = [
     'https://z2586300277.github.io/editorJson/v2/霸王龙百科.json',
+    'https://z2586300277.github.io/editorJson/v2/无人机定位动画.json',
     'https://z2586300277.github.io/editorJson/v2/三维测量.json',
     'https://z2586300277.github.io/editorJson/v2/mirror.json'
 ]
