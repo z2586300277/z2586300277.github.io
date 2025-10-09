82d6ec997c7e987c36e46616ae1039b14dd8f143
zhangjingguang <2586300277@qq.com>
2025-10-09 17:58:59 +0800

    v2

--- public/editorJson/v2.js
+++ public/editorJson/v2.js
@@ -1,4 +1,5 @@
 window.addon_editor_json = [
+    'https://z2586300277.github.io/editorJson/v2/蓝色科技地图.json',
     'https://z2586300277.github.io/editorJson/v2/霸王龙百科.json',
     'https://z2586300277.github.io/editorJson/v2/着色器组件磨砂反射.json',
     'https://z2586300277.github.io/editorJson/v2/无人机定位动画.json',
