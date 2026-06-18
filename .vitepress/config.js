6a02d95512cb749b3d82c0611982cd48811e527b
zhangjingguang <2586300277@qq.com>
2026-06-18 09:30:47 +0800

    config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -31,7 +31,7 @@ export default defineConfig({
       { text: '二维码📱', link: '/personalCode.md' },
       { text: '3D展示🍂', link: '/works.md' },
       { text: '资源💙', link: '/resource.md' },
-      { text: '留言📝', link: '/message.md' },
+      { text: '简介🐸', link: '/message.md' },
       { text: '赞赏👍', link: '/sponsor.md' },
       { text: 'BiBi📺', link: 'https://space.bilibili.com/245165721' },
       { text: '闲鱼🐟', link: 'https://www.goofish.com/personal?userId=2885508577' },
@@ -43,7 +43,7 @@ export default defineConfig({
       '/examples/': examplesSidebar
     },
     footer: {
-      copyright: `<img src="https://visitor-badge.laobi.icu/badge?page_id=z2586300277" style="float:left" > 版权所有 ©2019-${new Date().getFullYear()} 优雅永不过时 `
+      copyright: `<img src="https://visitor-badge.laobi.icu/badge?page_id=z2586300277" style="float:left" > 版权所有 ©2019-${new Date().getFullYear()} 2586300277@qq.com 优雅永不过时 `
     },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/z2586300277' }
